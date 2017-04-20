72d3d8775d89cf709bed6da0be41babfa84cc434
mmfilesi <mmfilesi@gmail.com>
2017-04-20 15:56:29 +0200

    demo-animals02

--- src/app/services/animals-factory.js
+++ src/app/services/animals-factory.js
@@ -7,9 +7,12 @@
     var self = module;
 
     var allAnimals = [
-        {id: 0, name: 'Unicornios'},
-        {id: 1, name: 'Pitufos'},
-        {id: 2, name: 'Gatos'},
+        {id: 0, name: 'Gatos', img:'cats.jpg'},
+        {id: 1, name: 'Camaleones', img:'camaleones.jpg'},
+        {id: 2, name: 'Perros', img: 'dogs.jpg'},
+        {id: 3, name: 'Hipopótamos', img: 'hipo.jpg'},
+        {id: 4, name: 'Cigüeñas', img: 'ciguenas.jpg'},
+        {id: 5, name: 'Pulpos', img: 'pulpo.jpg'},
     ];
 
     module.getAllAnimals = function() {
